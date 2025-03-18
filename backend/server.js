da7acaa2ac87f809f2f967d5a5405cb8e0fdcf4a
jayesh pj <jayeshpj343@gmail.com>
2025-03-18 16:53:02 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,13 @@ app.use('/api/tasks', taskRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// ✅ Serve static files from React's build folder
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// // ✅ Serve static files from React's build folder
+// app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-// ✅ Catch-all handler to serve React's index.html for any route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-});
+// // ✅ Catch-all handler to serve React's index.html for any route
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
